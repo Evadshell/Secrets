b3f9488f91a13c4180c1446a8dcabdb3aca9b5ce
tejas <keshwanitejas@gmail.com>
2024-04-21 00:50:19 +0530

    updated

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
 import env from "dotenv";
+const { Pool } = pg;
 
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL ,
+})
 const app = express();
 const port = 3000;
 const saltRounds = 10;
