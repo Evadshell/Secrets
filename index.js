808fa2927025988e32d282e3d9d2850173d7162d
Evadshell <evadshell69@gmail.com>
2024-04-22 14:42:51 +0530

    updates basic app

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: [
         "http://localhost:3000/auth/google/secrets",
-        "https://secrets-hfnzm7bs1-evadshells-projects.vercel.app/auth/google/secrets"
+        "https://secrets-phi.vercel.app/auth/google/secrets"
       ],
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
       passReqToCallback : true,
