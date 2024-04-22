875d23edf9568f47a136c574499a5b564737405a
Evadshell <evadshell69@gmail.com>
2024-04-22 17:44:46 +0530

    fixing css

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.use(
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,'public')));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); 
 
